8a387bd770b58100d85c1fe3e632c33d39100f84
Xufor <lucky1612000@gmail.com>
2019-04-14 21:37:55 +0530

    0025485

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const connect = require('knex');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors());
 
 const db = connect({
     client: 'pg',
